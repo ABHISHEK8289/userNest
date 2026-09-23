9356b0fab25ab2eaa8bf7cc06d18d882c66205d6
abhishek <abhisheksabhi1116@gmail.com>
2026-09-23 14:56:42 +0530

    Initial UserPortal project

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,7 @@ nestServer.use(cors());
 nestServer.use(express.json());
 nestServer.use("/api", router);
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 nestServer.listen(PORT, () => {
   console.log("nestServer Started...... And Waiting for Client Request");
 });
